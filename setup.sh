428f6e706d0ebf862aa767baf8081c723576c415
wudilab <wudilinux@gmail.com>
2017-12-10 22:31:27 +1100

    add webapi xunit test

---
+++ setup.sh
@@ -0,0 +1,14 @@
+dotnet new sln
+mkdir webapi
+cd webapi
+dotnet new webapi
+dotnet add package Autofac
+dotnet add package Autofac.Extensions.DependencyInjection
+dotnet add package MongoDB.Driver
+
+cd ..
+mkdir webapi_test
+dotnet new xunit
+
+dotnet sln webapi.sln add webapi/webapi.csproj
+dotnet sln webapi.sln add webapi_test/webapi_test.csproj
